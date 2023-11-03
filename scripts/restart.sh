3ea79e0f77e596347d6546fe94e284018112426f
Rohan Hussain <22100063@lums.edu.pk>
2023-11-03 21:44:19 +0500

    nvm export in restart.sh

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install v18.18.2
 node --version
 npm install -g pm2
